5fc6770ae5391b1faec1cb365abe589b617c2a1b
samreensaif <samreensaif81@gmail.com>
2025-03-19 07:48:13 +0000

    fun sentence

---
+++ 01_expressions/08_fun-sentence.py
@@ -0,0 +1,12 @@
+enter_adjective = "Enter an adjective: "
+enter_noun = "Enter a noun: "
+enter_verb = "Enter a verb: "
+
+adjective = input(enter_adjective)
+noun = input(enter_noun)
+verb = input(enter_verb)
+
+
+sample_sentence = "Code in Place is fun. I learned to program and used Python to make my"
+
+print (f"{sample_sentence} {adjective} {noun} {verb}!")
