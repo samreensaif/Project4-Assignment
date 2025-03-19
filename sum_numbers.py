9b7c344635cb8995911d5e1fb46ff380aaa6210d
samreensaif <samreensaif81@gmail.com>
2025-03-19 06:16:23 +0000

    First commit - Added sum_numbers.py

---
+++ sum_numbers.py
@@ -0,0 +1,10 @@
+
+question_1='enter the first number: '
+question_2='enter the second number: '
+
+first_number=input(question_1)
+first_number=int(first_number)
+second_number=input(question_2)
+second_number=int(second_number)
+total_sum=first_number+second_number
+print(f"""The total number of {first_number} and {second_number} is {total_sum}""")
