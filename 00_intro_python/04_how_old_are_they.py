df81a66811b978f818bc9b8f45aa082676296f6e
samreensaif <samreensaif81@gmail.com>
2025-03-19 07:11:11 +0000

    First commit - how old are they

---
+++ 00_intro_python/04_how_old_are_they.py
@@ -0,0 +1,13 @@
+anton_age = 21
+beth_age = anton_age + 6
+chen_age = beth_age + 20
+drew_age = chen_age + anton_age
+ethen_age=chen_age
+
+print(f"""Age-Related Riddle\n
+Anton is {anton_age}\n
+Beth is {beth_age}\n
+Chen is {chen_age}\n
+Drew is {drew_age}\n
+Ethen is {ethen_age}
+           """)
