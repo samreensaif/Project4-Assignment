c5481b9ffc3256197d7fc1449de1af382a99b077
samreensaif <samreensaif81@gmail.com>
2025-03-19 07:47:18 +0000

    seconds in a year

---
+++ 01_expressions/07_seconds-in-a-year.py
@@ -0,0 +1,10 @@
+print("LETS CALCULATE NUMBER OF SECONDS IN A YEAR")
+
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+
+seconds_in_year = days_in_year * hours_in_day * minutes_in_hour * seconds_in_minute
+
+print(f'There are {seconds_in_year} seconds in a year')
