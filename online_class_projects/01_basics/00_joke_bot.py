02f58594014a4b9e70af4201e0cf02905e2dd112
samreensaif <samreensaif81@gmail.com>
2025-03-19 14:54:00 +0000

    Added 00_joke_bot.py

---
+++ online_class_projects/01_basics/00_joke_bot.py
@@ -0,0 +1,11 @@
+prompt="What Do You Want"
+joke="Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs"
+sorry="Sorry"
+
+print(prompt)
+user_input=input()
+
+if user_input == "joke":
+  print(joke)
+else:
+  print(sorry)
