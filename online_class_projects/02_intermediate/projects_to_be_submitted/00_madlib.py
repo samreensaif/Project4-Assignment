1be1d4ff95c1d795c0500a8f967cd130d5ea84d6
samreensaif <samreensaif81@gmail.com>
2025-03-19 19:57:31 +0000

    Project1

---
+++ online_class_projects/02_intermediate/projects_to_be_submitted/00_madlib.py
@@ -0,0 +1,17 @@
+
+adjective = input ("enter Adjective:")
+
+noun = input ("enter Noun:")
+
+place = input ("enter Place:")
+
+verb = input ("enter Verb:")
+
+animal = input ("enter Animal:")
+
+emotion = input ("enter Emotion:")
+
+print(f"""Ek din, {adjective} {noun} ne decide kiya ke wo {place} par jayega.
+Wahan pohanch kar usne {verb} karna shuru kiya. Achaanak, ek {adjective} {animal}
+wahan aaya aur {verb} karne laga. Sab log {emotion} ho gaye aur us din ko
+kabhi nahi bhool sake.""")
