bf6b5468e766289416925829a7e44a82e546a053
samreensaif <samreensaif81@gmail.com>
2025-03-19 15:31:57 +0000

    01_double_it

---
+++ online_class_projects/01_basics/01_double_it.py
@@ -0,0 +1,7 @@
+number = int (input ("Enter any Number you want to double: "))
+
+while number < 100:
+  print (f" {number} doubled is {number*2} \n")
+  number = number * 2
+
+print(f" {number} is greater than 100 so we stopped here")
