4a8682d9139adad90a883b7d72139fb437183685
samreensaif <samreensaif81@gmail.com>
2025-03-20 15:15:12 +0000

    project6

---
+++ online_class_projects/02_intermediate/projects_to_be_submitted/Project6_countdown_time.py
@@ -0,0 +1,11 @@
+import time
+
+count_time = int(input("Enter time in seconds: "))
+
+for i in range(count_time,0,-1):
+  sec = i % 60
+  min = int (i / 60) % 60
+  hr = int(i / 3600)
+  print (f"{hr:02}:{min:02}:{sec:02}")
+  time.sleep(1)
+print("times up!!!!")
