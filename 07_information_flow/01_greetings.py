6e3e1e8843de46a5375fb568dd2aaec559dac9cf
samreensaif <samreensaif81@gmail.com>
2025-03-23 17:16:16 +0000

    greetings

---
+++ 07_information_flow/01_greetings.py
@@ -0,0 +1,9 @@
+
+def greetings(name:str):
+
+  return(f"Greetings with ❤ {name} !!!")
+def main():
+  enter_name= input("enter your name: ")
+  print(greetings(enter_name))
+if __name__ == "__main__":
+  main()
